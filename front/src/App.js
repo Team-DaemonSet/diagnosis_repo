67c0fcf0eace5594671235f0113ab792f162f07d
whereiswilly <koreapower98@naver.com>
2024-05-09 00:50:25 +0900

    first push

--- front/src/App.js
+++ front/src/App.js
@@ -22,7 +22,7 @@ function App() {
     formData.append('file', file);
 
     try {
-      const response = await axios.post('http://localhost:5000/upload', formData, {
+      const response = await axios.post('/upload', formData, {
         headers: {
           'Content-Type': 'multipart/form-data'
         }
