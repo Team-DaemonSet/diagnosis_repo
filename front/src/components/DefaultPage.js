99b8a05a938833ac431206521211d7c435578e82
유태균 <yoc1256@nate.com>
2024-05-22 14:18:46 +0900

    3t

---
+++ front/src/components/DefaultPage.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const DefaultPage = () => (
+    <div className="default-page">
+        <h1>기본 페이지입니다.<hr></hr>기본 페이지라고 말했습니다.</h1>
+    </div>
+);
+
+export default DefaultPage;
