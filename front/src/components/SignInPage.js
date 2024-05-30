35e46f2a7c8493fbbf56ee43d0f28d70c129dc80
yoo-chris <chris4929@naver.com>
2024-05-30 23:27:50 +0900

    123123

--- front/src/components/SignInPage.js
+++ front/src/components/SignInPage.js
@@ -17,7 +17,7 @@ const SignInPage = ({ setIsAuthenticated }) => {
         setIsLoading(true);
 
         try {
-            const response = await axios.post('http://localhost:5012/signin', {
+            const response = await axios.post('http://backend-sercvice-1:5012/signin', {
                 email: email,
                 password: password
             }, {
